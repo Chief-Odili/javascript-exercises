c529d244c1438787e7839a6000cf9ce959a7a625
Chief-Odili <odilistanislaus@gmail.com>
2024-01-05 16:05:40 +0100

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
-
-// Do not edit below this line
-module.exports = removeFromArray;
+const removeFromArray = function(arr, ...elementsToRemove) {
+    // Create a new array without the specified elements
+    const filteredArray = arr.filter(function(item) {
+      // Include the item in the new array only if it's not in elementsToRemove
+      return !elementsToRemove.includes(item);
+    });
+    
+    // Return the new array without the specified elements
+    return filteredArray;
+  };
+  
+  // Do not edit below this line
+  module.exports = removeFromArray;
+  
