c16dcc9080489a879a6f0ec49f6ea077816c2186
Chief-Odili <odilistanislaus@gmail.com>
2024-01-09 18:15:42 +0100

    backend calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(numbers) {
+  return numbers.reduce((acc, num) => acc + num, 0);
 	
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+
 
 // Do not edit below this line
 module.exports = {
